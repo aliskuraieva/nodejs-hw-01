64d9c70b27a494b5232e90d8869f9548ba185c52
Alisa Kuraieva <alisa.mogila@gmail.com>
2024-12-03 11:42:02 +0200

    Update getAllContacts.js

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,5 @@
 import { readContacts } from '../utils/readContacts.js';
 
-export const getAllContacts = () => readContacts;
+export const getAllContacts = () => readContacts();
 
 console.log(await getAllContacts());
